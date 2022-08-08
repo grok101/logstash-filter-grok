281b7a0b75098b861a95de8447d6c9dc969941a4
Vinod Pahuja <vinodpahuja@users.noreply.github.com>
2022-08-08 14:48:05 +0530

    Delete .ci/performance directory

--- .ci/performance/docker-run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# This is intended to be run inside the docker container as the command of the docker-compose.
-set -ex
-
-cd .ci
-
-# docker will look for: "./docker-compose.yml" (and "./docker-compose.override.yml")
-docker-compose run logstash .ci/performance/run.sh
--- .ci/performance/docker-setup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# This is intended to be run inside the docker container as the command of the docker-compose.
-set -ex
-
-# docker will look for: "./docker-compose.yml" (and "./docker-compose.override.yml")
-.ci/docker-setup.sh
